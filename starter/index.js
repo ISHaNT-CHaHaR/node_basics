e0a240181fb44fda72465831f142fac6a67ce726
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2019-12-28 17:38:45 +0530

    asynchronous way.
    updated code for read and write in an asynchronous way.

--- starter/index.js
+++ starter/index.js
@@ -1,8 +1,18 @@
 const fs= require('fs');
-const textin = fs.readFileSync( './txt/input.txt','utf8');
-console.log(textin);
 
- const textout= `This is what we know about the avocado:${textin}`;
- fs.writeFileSync('./txt/output.txt',textout);
- console.log("file Written!");
+//Blocking synchronous  way 
+//const textin = fs.readFileSync( './txt/input.txt','utf8');
+//console.log(textin);
+
+ //const textout= `This is what we know about the avocado:${textin}`;
+ //fs.writeFileSync('./txt/output.txt',textout);
+ //console.log("file Written!");
   
+ // Non-Blocking Asynchronous way.
+
+fs.readFile('.txt/start.txt','utf-8', (err,data)=>{
+     console.log(data);
+
+}); 
+console.log('will read data');
+
