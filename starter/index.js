44595ed36bde7bfc14c945a5291141f817c154d4
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2019-12-28 15:51:00 +0530

    NOW one can read and write.
    The code is updated and now one can read and write using node.js in  synchrounous way.

--- starter/index.js
+++ starter/index.js
@@ -1,8 +1,8 @@
 const fs= require('fs');
-const textin = fs.readFileSync( './txt/input.txt','utf-8');
+const textin = fs.readFileSync( './txt/input.txt','utf8');
 console.log(textin);
 
- const textout= `This is what we know about the avocado:${textIn}.\nCreated on ${Date.now()}`;
- fs.writeFileSync('./txt/output.txt','utf-8',textout);
+ const textout= `This is what we know about the avocado:${textin}`;
+ fs.writeFileSync('./txt/output.txt',textout);
  console.log("file Written!");
   
